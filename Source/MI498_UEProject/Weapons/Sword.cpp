dbf28ce639ad8e0aa7c8d901cd136942f92d482d
AlexisBliesener <bliesen7@msu.edu>
2026-01-24 20:13:56 -0500

    Sword Prototype Implementation
    - Single attack
    - Dash

--- Source/MI498_UEProject/Weapons/Sword.cpp
+++ Source/MI498_UEProject/Weapons/Sword.cpp
@@ -1,6 +1,23 @@
 #include "Sword.h"
+#include "MI498_UEProject/Player/PlayerCharacter.h"
 
 void ASword::PrimaryAttack(APlayerController* PlayerController)
 {
 	Super::PrimaryAttack(PlayerController);
+}
+
+void ASword::SecondaryAttack(APlayerController* PlayerController)
+{
+	/// Get the player camera location and rotation for dash direction
+	FVector cameraLocation;
+	FRotator cameraRotation;
+	PlayerController->GetPlayerViewPoint(cameraLocation, cameraRotation);
+	FVector cameraForwardVector = cameraRotation.Vector();
+	
+	/// Get a reference to the owning player character
+	APlayerCharacter* playerCharacter = Cast<APlayerCharacter>(GetOwner());
+	
+	/// Dashes the player forward in look direction
+	FVector launchVelocity = cameraForwardVector * DashForce;
+	playerCharacter->LaunchCharacter(launchVelocity, true, true);
 }
--- Source/MI498_UEProject/Weapons/Sword.h
+++ Source/MI498_UEProject/Weapons/Sword.h
@@ -5,14 +5,24 @@
 #include "GameFramework/Actor.h"
 #include "Sword.generated.h"
 
-UCLASS(Blueprintable)
+/// Concrete weapon class representing a Sword
+/// /// Inherits from WeaponBase
+UCLASS()
 class MI498_UEPROJECT_API ASword : public AWeaponBase
 {
 
 public:
 	/// Override the PrimaryAttack function to implement Sword specific attacking behavior
-	/// @param PlayerController - Provides context about who is attaclomg 
+	/// @param PlayerController - Provides context about who is attacking 
 	virtual void PrimaryAttack(APlayerController* PlayerController) override;
+	
+	/// Override the Secondary Attack function to implement Sword specific attacking behavior
+	/// @param PlayerController - Provides context about who is attacking 
+	virtual void SecondaryAttack(APlayerController* PlayerController) override;
+	
+	/// How much force is applied to the player when dashing
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int DashForce = 700;
 
 protected:
 
