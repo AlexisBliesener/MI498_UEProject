4affc6a693c5eb856016cd4b878e67b421012ad3
Mohammed Alanizy <alanizym@msu.edu>
2026-02-03 02:44:43 -0500

    FIXED ENEMIES CAN'T ATTACK WHEN THEY ARE TOO MUCH

---
+++ Source/MI498_UEProject/AI/Evaluators/PlayerEvaluator.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerEvaluator.h"
+#include "Kismet/GameplayStatics.h"
+
+void UPlayerEvaluator::TreeStart(FStateTreeExecutionContext& Context)
+{
+	Super::TreeStart(Context);
+
+}
+
+void UPlayerEvaluator::TreeStop(FStateTreeExecutionContext& Context)
+{
+	Super::TreeStop(Context);
+	
+}
+
+void UPlayerEvaluator::Tick(FStateTreeExecutionContext& Context, const float DeltaTime)
+{
+	UWorld* World = Actor->GetWorld();
+	if (!World)
+	{
+		DistanceToPlayer = 0.0f;
+		return;
+	}
+
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(World, 0);
+	if (!PlayerPawn)
+	{
+		DistanceToPlayer = 0.0f;
+		return;
+	}
+	// cache the player
+	Player = PlayerPawn;
+	// calculate the distance to the player
+	DistanceToPlayer = FVector::Dist(Actor->GetActorLocation(), PlayerPawn->GetActorLocation());
+	
+	
+	// change if the player is in range for attack
+	bIsPlayerInRange = (DistanceToPlayer <= AttackRange);
+}
