30eee87f362b2bf7b587acebd3f3b55c85e56e77
AlexisBliesener <bliesen7@msu.edu>
2026-02-25 02:38:56 -0500

    Fix plugin build

--- Plugins/MGDynamicNavigation-master/Source/MGDynamicNavigation/MGDynamicNavigation.Build.cs
+++ Plugins/MGDynamicNavigation-master/Source/MGDynamicNavigation/MGDynamicNavigation.Build.cs
@@ -7,49 +7,39 @@ public class MGDynamicNavigation : ModuleRules
 	public MGDynamicNavigation(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
+
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
-				"Core","CoreUObject","Engine","InputCore","NavigationSystem","AIModule",
-				"GameplayTasks","Navmesh"
-				// ... add other public dependencies that you statically link with here ...
+				"Core",
+				"CoreUObject",
+				"Engine",
+				"InputCore",
+				"NavigationSystem",
+				"AIModule",
+				"GameplayTasks",
+				"Navmesh"
 			}
-			);
-			
-		
+		);
+
 		PrivateDependencyModuleNames.AddRange(
 			new string[]
 			{
 				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore", 
-				"UnrealEd"
-				// ... add private dependencies that you statically link with here ...	
+				"Engine"
 			}
-			);
+		);
 		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
+		if (Target.bBuildEditor)
+		{
+			PrivateDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"Slate",
+					"SlateCore",
+					"UnrealEd"
+				}
 			);
+		}
 	}
 }
