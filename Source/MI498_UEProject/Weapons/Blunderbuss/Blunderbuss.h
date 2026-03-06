f10eec48febd49a5eedce5bd632fffcef261a456
AlexisBliesener <bliesen7@msu.edu>
2026-03-06 13:13:22 -0500

    remove unneeded variable

--- Source/MI498_UEProject/Weapons/Blunderbuss/Blunderbuss.h
+++ Source/MI498_UEProject/Weapons/Blunderbuss/Blunderbuss.h
@@ -39,10 +39,6 @@ public:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	int SecondaryAttackKnockbackForce = 1000;
 	
-	/// Vertical camera recoil applied to the player when firing
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	int CameraRecoil = -5;
-	
 	/// Damage multiplier applied to the secondary (double) shot
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	float DoubleShotDamageMultiplier = 2;
