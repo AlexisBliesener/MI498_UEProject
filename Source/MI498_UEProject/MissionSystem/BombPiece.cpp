4d97374131e5f4c954ec4d7f34d8d0d31cf0f1c4
DBassettGameDev <dylanbassettgamedev@gmail.com>
2026-04-21 22:30:49 -0400

    Bomb Piece Pickup Lerp

--- Source/MI498_UEProject/MissionSystem/BombPiece.cpp
+++ Source/MI498_UEProject/MissionSystem/BombPiece.cpp
@@ -4,5 +4,5 @@ void ABombPiece::BombPieceCollected(int32 index)
 {
 	/// Broadcast the collection event then destory the bomb
 	OnBombPieceCollected.Broadcast(index);
-	Destroy();
+	PocketBombPiece();
 }
